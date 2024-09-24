8793bc483488282d50b282b97791e39bfd9a9c30
Amalia Kivansa Anggreansi <72345090+amaliakivansa@users.noreply.github.com>
2024-09-24 10:04:24 +0700

    add routing & middleware

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  const mahasiswa = [
+    {
+      nama: 'Amalia',
+      email: 'amalia@gmail.com',
+    },
+    {
+      nama: 'Budi',
+      email: 'budi@gmail.com',
+    },
+    {
+      nama: 'Charlie',
+      email: 'charlie@gmail.com',
+    },
+  ];
+  res.render('index', {
+    title: 'Halaman Home',
+    layout: 'layouts/main-layout',
+    nama: 'Amalia',
+    mahasiswa,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'Halaman About',
+    layout: 'layouts/main-layout',
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    title: 'Halaman Contact',
+    layout: 'layouts/main-layout',
+  });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('404 Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
